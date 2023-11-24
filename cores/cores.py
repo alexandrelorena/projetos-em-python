a7a8b31df8331a451efc5cdb6099aebc05233339
ALEXANDRE LORENA <138406816+alexandrelorena@users.noreply.github.com>
2023-11-24 01:49:24 -0300

    Update cores.py

--- cores/cores.py
+++ cores/cores.py
@@ -1,33 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from colorama import init
-# from rich.console import Console
+from colorama import init, Fore
+from rich.console import Console
 from PIL import Image, ImageDraw
 import os
 import cores_unificadas
 
 # Inicializa o colorama
 init(autoreset=True)
-
-# def imprimir_com_cor(nome_cor, codigo_hex):
-#     try:
-#         # Obtém a cor formatada
-#         cor_formatada = getattr(Fore, nome_cor.upper())
-#         # Obtém os valores RGB
-#         rgb = tuple(int(codigo_hex[i:i + 2], 16) for i in (1, 3, 5))
-#
-#         # Imprime o nome da cor com a cor e as informações de hexadecimal e RGB
-#         print(f"{cor_formatada}{nome_cor}", end="")
-#
-#         # Imprime as informações de hexadecimal e RGB usando colorama
-#         print(
-#             f" - {Fore.YELLOW}Hex:{Fore.RESET} {Fore.CYAN}{codigo_hex}{Fore.RESET} - {Fore.GREEN}RGB:{Fore.RESET} {Fore.CYAN}{rgb}{Fore.RESET}")
-#     except AttributeError:
-#         # Se a cor não for suportada, imprime apenas o valor hexadecimal e RGB
-#         rgb = tuple(int(codigo_hex[i:i + 2], 16) for i in (1, 3, 5))
-#         print(f"{nome_cor} não suportada: Hex: {codigo_hex} - RGB: {rgb}")
-
 
 # Tamanho da imagem
 width = 350
@@ -55,104 +36,100 @@ for nome_cor, cor_info in cores_unificadas.cores_unificadas.items():
     except Exception as e:
         print(f"Erro ao salvar a imagem para {nome_cor}: {e}")
 
+# ... (o restante do código permanece inalterado)
+
 # Agora, vamos criar o arquivo HTML
-html_content = """
-<!DOCTYPE html>
+html_content = """<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-<script>
-    // Função para escolher uma cor aleatória da lista
-    function escolherCorAleatoria() {
-        // Obtém todas as células de cor disponíveis
+    <script>
+        // Função para escolher uma cor aleatória da lista
+        function escolherCorAleatoria() {
+            // Obtém todas as células de cor disponíveis
+            var colorCells = document.querySelectorAll('.light-text');
+
+            // Lista de cores disponíveis
+            var cores = [];
+
+            // Obtém as cores da lista de células
+            colorCells.forEach(function(cell) {
+                var color = cell.style.color;
+                cores.push(color);
+            });
+
+            // Escolhe uma cor aleatória da lista
+            var corAleatoria = cores[Math.floor(Math.random() * cores.length)];
+
+            // Aplica a cor ao fundo da página
+            document.body.style.backgroundColor = corAleatoria;
+        }
+
+        // Chama a função ao carregar a página
+        window.onload = escolherCorAleatoria;
+
+        // Adiciona um ouvinte de evento para cada célula de cor
         var colorCells = document.querySelectorAll('.light-text');
 
-        // Lista de cores disponíveis
-        var cores = [];
-
-        // Obtém as cores da lista de células
         colorCells.forEach(function(cell) {
-            var color = cell.style.color;
-            cores.push(color);
+            // Mouseover para mudar a cor do cursor
+            cell.addEventListener('mouseover', function() {
+                var color = cell.style.color;
+                changeMouseColor(color);
+            });
+            // Mouseout para restaurar a cor padrão do cursor
+            cell.addEventListener('mouseout', function() {
+                resetMouseColor();
+            });
+
+            // Click para mudar a cor de fundo da página
+            cell.addEventListener('click', function() {
+                var color = cell.style.color;
+                changeBackgroundColor(color);
+            });
         });
 
-        // Escolhe uma cor aleatória da lista
-        var corAleatoria = cores[Math.floor(Math.random() * cores.length)];
+        // Adiciona um ouvinte de evento para cada célula de imagem
+        var imageCells = document.querySelectorAll('td img');
 
-        // Aplica a cor ao fundo da página
-        document.body.style.backgroundColor = corAleatoria;
-    }
-
-    // Chama a função ao carregar a página
-    window.onload = escolherCorAleatoria;
-
-    // Adiciona um ouvinte de evento para cada célula de cor
-    var colorCells = document.querySelectorAll('.light-text');
-
-    colorCells.forEach(function(cell) {
-        // Mouseover para mudar a cor do cursor
-        cell.addEventListener('mouseover', function() {
-            var color = cell.style.color;
-            changeMouseColor(color);
-        });
-        // Mouseout para restaurar a cor padrão do cursor
-        cell.addEventListener('mouseout', function() {
-            resetMouseColor();
+        imageCells.forEach(function(cell) {
+            // Mouseover para mudar a cor do cursor
+            cell.addEventListener('mouseover', function() {
+                var color = cell.parentNode.previousElementSibling.style.color;
+                changeMouseColor(color);
+            });
+            // Mouseout para restaurar a cor padrão do cursor
+            cell.addEventListener('mouseout', function() {
+                resetMouseColor();
+            });
+            // Click para mudar a cor de fundo da página
+            cell.addEventListener('click', function() {
+                var color = cell.parentNode.previousElementSibling.style.color;
+                changeBackgroundColor(color);
+            });
         });
 
-        // Click para mudar a cor de fundo da página
-        cell.addEventListener('click', function() {
-            var color = cell.style.color;
-            changeBackgroundColor(color);
-        });
-    });
+        // Função para mudar a cor do cursor
+        function changeMouseColor(color) {
+            document.body.style.cursor = `url("data:image/svg+xml,%3Csvg fill='${color}' height='24' width='24' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='12' cy='12' r='10'/%3E%3C/svg%3E"), auto`;
+        }
+        // Função para restaurar a cor padrão do cursor
+        function resetMouseColor() {
+            document.body.style.cursor = 'auto';
+        }
 
-    // Adiciona um ouvinte de evento para cada célula de imagem
-    var imageCells = document.querySelectorAll('td img');
-
-    imageCells.forEach(function(cell) {
-        // Mouseover para mudar a cor do cursor
-        cell.addEventListener('mouseover', function() {
-            var color = cell.parentNode.previousElementSibling.style.color;
-            changeMouseColor(color);
-        });
-        // Mouseout para restaurar a cor padrão do cursor
-        cell.addEventListener('mouseout', function() {
-            resetMouseColor();
-        });
-
-        // Click para mudar a cor de fundo da página
-        cell.addEventListener('click', function() {
-            var color = cell.parentNode.previousElementSibling.style.color;
-            changeBackgroundColor(color);
-        });
-    });
-
-// Função para mudar a cor do cursor
-function changeMouseColor(color) {
-    document.body.style.cursor = `url("data:image/svg+xml,%3Csvg height='0' width='0' xmlns='http://www.w3.org/2000/svg'%3E%3C/style%3E%3C/svg%3E"), auto`;
-    document.body.style.setProperty('--cursor-color', color);
-}
-    // Função para restaurar a cor padrão do cursor
-    function resetMouseColor() {
-        document.body.style.cursor = 'auto';
-    }
-
-    // Função para mudar a cor de fundo da página
-    function changeBackgroundColor(color) {
-        document.body.style.backgroundColor = color;
-    }
-</script>
-
-
-
+        // Função para mudar a cor de fundo da página
+        function changeBackgroundColor(color) {
+            document.body.style.backgroundColor = color;
+        }
+    </script>
     <style>
         body {
             background-color: #eeeeee; /* Cor de fundo do corpo da página */
             font-family: Arial, sans-serif;
             letter-spacing: 1px; /* Ajuste o valor conforme necessário */
         }
-        
+
         table {
             font-family: Arial, sans-serif;
             letter-spacing: 1px; /* Ajuste o valor conforme necessário */
@@ -163,7 +140,6 @@ function changeMouseColor(color) {
         }
         th, td {
             border: 1px solid #dddddd;
-            text-align: left;
             padding: 8px;
         }
         th {
@@ -171,6 +147,9 @@ function changeMouseColor(color) {
             position: sticky;
             top: 0;
             z-index: 1;
+            vertical-align: middle; /* Centraliza o texto verticalmente nas células de cabeçalho */
+            text-align: center; /* Centraliza o texto horizontalmente nas células de cabeçalho */
+            color: gray; /* Cor do texto no cabeçalho */
         }
         img {
             width: 100%;
@@ -180,23 +159,25 @@ function changeMouseColor(color) {
             font-weight: lighter;
             text-shadow: 0.7px 0.7px 1px #000000; /* Ajuste os valores conforme necessário */
         }
-            /* Adiciona um estilo personalizado para a cor do cursor */
-    body {
-        cursor: url("data:image/svg+xml,%3Csvg height='0' width='0' xmlns='http://www.w3.org/2000/svg'%3E%3C/style%3E%3C/svg%3E"), auto;
-    }
+        /* Adiciona um estilo personalizado para a cor do cursor */
+        body {
+            cursor: url("data:image/svg+xml,%3Csvg height='0' width='0' xmlns='http://www.w3.org/2000/svg'%3E%3C/style%3E%3C/svg%3E"), auto;
+        }
 
-    body:hover {
-        cursor: url("data:image/svg+xml,%3Csvg fill='var(--cursor-color)' height='24' width='24' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='12' cy='12' r='10'/%3E%3C/svg%3E"), auto;
-    }
+        body:hover {
+            cursor: url("data:image/svg+xml,%3Csvg fill='var(--cursor-color)' height='24' width='24' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='12' cy='12' r='10'/%3E%3C/svg%3E"), auto;
+        }
     </style>
 </head>
 <body>
 
-<table>
-    <tr>
-        <th style="width: 350px;"><font color="Gray"> :::::::::::::::::: COR | HEXA | RGB :::::::::::::::::: </font></th>
-        <th><font color="Gray"> :::::::::::: REPRESENTAÇÃO DA COR :::::::::::: </font></th>
-    </tr>
+    <table style="width: 700px;">
+        <tr> 
+            <th style=" font-size: 12px;" colspan="2">
+                <div style="display: inline-block;">:::::::::::::::::::::::::::::::::::::::::::::::::::::: 
+                PALETA DE CORES ::::::::::::::::::::::::::::::::::::::::::::::::::::::</div>
+            </th>
+        </tr>
 """
 
 # Adiciona as linhas da tabela ao HTML
@@ -207,64 +188,65 @@ for nome_cor, cor_info in cores_unificadas.cores_unificadas.items():
     # Adicione a classe 'light-text' para adicionar uma borda ao texto
     html_content += f"""
     <tr>
-        <td style="font-size: 15px; color: {cor_hex};" class="light-text">{nome_cor} | {cor_hex} | {cor_rgb}</td>
+        <td style="width: 350px; font-size: 13px; color: {cor_hex};" class="light-text">{nome_cor} | {cor_hex} | {cor_rgb}</td>
         <td style="width: 350px; height: 20px;"><img src="images/imagem_{nome_cor}.png" alt="{nome_cor}" height="20"></td>
     </tr>
-    
+
 """
 
 # Fecha o HTML
 html_content += """
-</table>
-<!-- Adicione o script JavaScript -->
-<script>
-    // Adicione um ouvinte de evento para cada célula de cor
-    var colorCells = document.querySelectorAll('.light-text');
+    </table>
+    <!-- Adicione o script JavaScript -->
+    <script>
 
-    colorCells.forEach(function(cell) {
-        // Mouseover para mudar a cor do cursor
-        cell.addEventListener('mouseover', function() {
-            var color = cell.style.color;
-            changeMouseColor(color);
+        // Adicione um ouvinte de evento para cada célula de cor
+        var colorCells = document.querySelectorAll('.light-text');
+
+        colorCells.forEach(function(cell) {
+            // Mouseover para mudar a cor do cursor
+            cell.addEventListener('mouseover', function() {
+                var color = cell.style.color;
+                changeMouseColor(color);
+            });
+
+            // Click para mudar a cor de fundo da página
+            cell.addEventListener('click', function() {
+                var color = cell.style.color;
+                changeBackgroundColor(color);
+            });
         });
 
-        // Click para mudar a cor de fundo da página
-        cell.addEventListener('click', function() {
-            var color = cell.style.color;
-            changeBackgroundColor(color);
-        });
-    });
+        // Adicione um ouvinte de evento para cada célula de imagem
+        var imageCells = document.querySelectorAll('td img');
 
-    // Adicione um ouvinte de evento para cada célula de imagem
-    var imageCells = document.querySelectorAll('td img');
-
-    imageCells.forEach(function(cell) {
-        // Mouseover para mudar a cor do cursor
-        cell.addEventListener('mouseover', function() {
-            var color = cell.parentNode.previousElementSibling.style.color;
-            changeMouseColor(color);
+        imageCells.forEach(function(cell) {
+            // Mouseover para mudar a cor do cursor
+            cell.addEventListener('mouseover', function() {
+                var color = cell.parentNode.previousElementSibling.style.color;
+                changeMouseColor(color);
+            });
+            // Mouseout para restaurar a cor padrão do cursor
+            cell.addEventListener('mouseout', function() {
+                resetMouseColor();
+            });
+            // Click para mudar a cor de fundo da página
+            cell.addEventListener('click', function() {
+                var color = cell.parentNode.previousElementSibling.style.color;
+                changeBackgroundColor(color);
+            });
         });
-        // Mouseout para restaurar a cor padrão do cursor
-        cell.addEventListener('mouseout', function() {
-            resetMouseColor();
-        });
-        // Click para mudar a cor de fundo da página
-        cell.addEventListener('click', function() {
-            var color = cell.parentNode.previousElementSibling.style.color;
-            changeBackgroundColor(color);
-        });
-    });
 
-    // Função para mudar a cor do cursor
-    function changeMouseColor(color) {
-        document.body.style.cursor = `url("data:image/svg+xml,%3Csvg fill='${color}' height='24' width='24' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='12' cy='12' r='10'/%3E%3C/svg%3E"), auto`;
-    }
+        // Função para mudar a cor do cursor
+        function changeMouseColor(color) {
+            document.body.style.cursor = `url("data:image/svg+xml,%3Csvg fill='${color}' height='24' width='24' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='12' cy='12' r='10'/%3E%3C/svg%3E"), auto`;
+        }
 
-    // Função para mudar a cor de fundo da página
-    function changeBackgroundColor(color) {
-        document.body.style.backgroundColor = color;
-    }
-</script>
+        // Função para mudar a cor de fundo da página
+        function changeBackgroundColor(color) {
+            document.body.style.backgroundColor = color;
+        }
+    </script>
 </body>
 </html>
 """
